324c0ef19e6a6f02d184c1e95bb7525e5d0f4a4a
Ana Priscila Cortez Gutierrez <anaprisy@gmail.com>
2020-05-26 13:42:42 -0500

    prueba

---
+++ Controllers/PruebaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPlantilla.Controllers
+{
+    public class PruebaController : Controller
+    {
+        //
+        // GET: /Prueba/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
